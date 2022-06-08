2a4ecc94377cdf8f1917eb995a3a878763e57fe0
StrawbWine <severin.hanevik@gmail.com>
2022-06-08 22:00:58 +0200

    ✨Create and register InMemoryDataStore

---
+++ life-manager-backend/InMemoryDataStore.cs
@@ -0,0 +1,33 @@
+﻿using life_manager_backend.Models;
+
+namespace life_manager_backend
+{
+    public class InMemoryDataStore
+    {
+        public List<FoodDto> Foods;
+
+        public InMemoryDataStore()
+        {
+            Foods = CreateFoodData();
+        }
+
+        private List<FoodDto> CreateFoodData()
+        {
+            var foods = new List<FoodDto>();
+            foods.Add(new FoodDto()
+            {
+                Id = 1,
+                Name = "Spaghetti a la Capri",
+                Energy = 99,
+                Fat = 4.10,
+                SaturatedFat = 0.90,
+                Carbohydrates = 12.0,
+                Sugars = 2.60,
+                Fiber = 0.0,
+                Protein = 3.50,
+                Salt = 0.85
+            });
+            return foods;
+        }
+    }
+}
--- life-manager-backend/Program.cs
+++ life-manager-backend/Program.cs
@@ -12,6 +12,7 @@ namespace life_manager_backend
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<InMemoryDataStore>();
 
             var app = builder.Build();
 

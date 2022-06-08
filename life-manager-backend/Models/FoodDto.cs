3fafd30215daf62eaccc24f65f870d217a690031
StrawbWine <severin.hanevik@gmail.com>
2022-06-08 21:32:38 +0200

    ✨Add FoodDto class

---
+++ life-manager-backend/Models/FoodDto.cs
@@ -0,0 +1,18 @@
+﻿namespace life_manager_backend.Models
+{
+    public class FoodDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public int? Energy { get; set; }
+        public int? Fat { get; set; }
+        public int? SaturatedFat { get; set; }
+        public int? MonoUnsaturatedFat { get; set; }
+        public int? PolyUnsaturatedFat { get; set; }
+        public int? Carbohydrates { get; set; }
+        public int? Sugars { get; set; }
+        public int? Protein { get; set; }
+        public int? Salt { get; set; }
+        public int? Fiber { get; set; }
+    }
+}

5de52f2a2a62a067195adc05863d16794be13d0b
StrawbWine <severin.hanevik@gmail.com>
2022-06-08 22:01:36 +0200

    ✨Add basic GET food endpoint

---
+++ life-manager-backend/Controllers/FoodController.cs
@@ -0,0 +1,24 @@
+﻿using life_manager_backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace life_manager_backend.Controllers
+{
+    [Route("api/food")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly InMemoryDataStore _dataStore;
+
+        public FoodController(InMemoryDataStore dataStore)
+        {
+            _dataStore = dataStore ?? throw new ArgumentNullException(nameof(dataStore));
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FoodDto>>> GetFoods()
+        {
+            var foods = _dataStore.Foods;
+            return Ok(foods);
+        }
+    }
+}

6e8ef35f14dfe58d635644e3cba437ed01175675
StrawbWine <severin.hanevik@gmail.com>
2022-06-09 10:24:18 +0200

    ✨Add Food Entity

---
+++ life-manager-backend/Entities/Food.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace life_manager_backend.Entities
+{
+    public class Food
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; } = String.Empty;
+        [Range(0.0, double.MaxValue)]
+        public double? Energy { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? Fat { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? SaturatedFat { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? MonoUnsaturatedFat { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? PolyUnsaturatedFat { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? Carbohydrates { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? Sugars { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? Protein { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? Salt { get; set; }
+        [Range(0.0, double.MaxValue)]
+        public double? Fiber { get; set; }
+
+        public Food(string name)
+        {
+            Name = name;
+        }
+    }
+}

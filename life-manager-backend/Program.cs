47666901b137c7cca94cdc6374dffe7a91a5bd5c
StrawbWine <54211990+StrawbWine@users.noreply.github.com>
2022-07-17 11:45:45 +0200

    🔧Connect to Cosmos DB through Azure Key Vault credentials (#2)
    * ✨Add support for Cosmos DB and adapt previous models and controllers to Cosmos DB setup
    
    * 🔧Connect to Cosmos DB with key vault credentials

--- life-manager-backend/Program.cs
+++ life-manager-backend/Program.cs
@@ -1,13 +1,35 @@
 using life_manager_backend.DbContexts;
 using life_manager_backend.Services;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 
 namespace life_manager_backend
 {
     public class Program
     {
         public static void Main(string[] args)
-        {            
+        {
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                     }
+            };
+            var client = new SecretClient(new Uri("https://guitar-app-kv.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret connStringSecret = client.GetSecret("cosmos-connection-string");
+            KeyVaultSecret databaseSecret = client.GetSecret("cosmos-database");
+
+            string connString = connStringSecret.Value;
+            string databaseName = databaseSecret.Value;
+
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -36,8 +58,10 @@ namespace life_manager_backend
 
             builder.Services.AddDbContextFactory<CosmosContext>(optionsBuilder => {
                 optionsBuilder.UseCosmos(
-                    connectionString: builder.Configuration["DEV_COSMOS_CONNECTION_STRING"],
-                    databaseName: builder.Configuration["DEV_COSMOS_DATABASE"]
+                    //connectionString: builder.Configuration["DEV_COSMOS_CONNECTION_STRING"],
+                    //databaseName: builder.Configuration["DEV_COSMOS_DATABASE"]
+                    connectionString: connString,
+                    databaseName: databaseName
                 );
                 }
             );
@@ -47,7 +71,7 @@ namespace life_manager_backend
             //builder.Services.AddScoped<IFoodRepository, FoodRepository>();
             builder.Services.AddScoped<IFoodRepository, CosmosRepository>();
 
-            var app = builder.Build();
+            var app = builder.Build();            
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
